b1c6415d35723f6a0d8ef14d86b6c52d7251f68f
Anastasiia Kazimirova <stacykazimirova@gmail.com>
2022-03-17 08:13:25 +0300

    Adds folders of Django standard project

--- myfirst/myfirst/settings.py
+++ myfirst/myfirst/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
